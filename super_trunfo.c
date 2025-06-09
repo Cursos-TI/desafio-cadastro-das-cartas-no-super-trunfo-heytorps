f3aed5993735a55addb3553ff666d1f28a865dd7
heytorps <heytorsilva83@gmail.com>
2025-06-09 17:54:03 +0000

    criei o super super_trunfo

---
+++ super_trunfo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>;
+
+int main(){
+    printf("Desafio Super Trunfo!\n");
+
+    carta 1 :
+    char estado: A;
+    char codigo: A01;
+    char cidade: São Paulo;
+    int população: 11.895.578;
+    float area: 1.521 km²;
+    float pib: 828,98 bilhões de reais;
+    int pontos turisticos: 200;
+
+    carta 2 :
+    char estado: B;
+    char codigo: B02;
+    char cidade: Rio de Janeiro;
+    int população: 6.211.223;
+    float area: 1.200 km²;
+    float pib: 359,64 bilhões de reais;
+    int pontos turisticos: 150;
